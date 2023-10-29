491a15cd87d7510b8dbd859618b868f42dfa1c02
vaishprakhar19 <vaishprakhar19@gmail.com>
2023-10-29 10:11:53 +0530

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import { useState } from 'react';
 import './App.css';
 import Home from "./components/Home";
-import {auth,provider} from "./firebase-config"
-import {signInWithPopup, signOut} from "firebase/auth"
+import { auth, provider } from "./firebase-config"
+import { signInWithPopup, signOut } from "firebase/auth"
 
 function App() {
 
-  const [isLoggedIn,setIsLoggedIn]=useState(localStorage.getItem("isLoggedIn"));
-  const login=async ()=>{
-    await signInWithPopup(auth,provider)
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem("isLoggedIn"));
+  let login, logout;
+  try {
+    login = async () => {
+      await signInWithPopup(auth, provider)
       setIsLoggedIn(true);
-      localStorage.setItem("isLoggedIn","true");
-  }
+      localStorage.setItem("isLoggedIn", true);
+    }
 
-  const logout = async ()=>{
-    await signOut(auth);
-    setIsLoggedIn(false);
-    localStorage.setItem("isLoggedIn",false);
+    logout = async () => {
+      await signOut(auth);
+      setIsLoggedIn(false);
+      localStorage.clear();
+    }
   }
+  catch (err) { console.log(err); }
 
   return (
     <div className="App">
       <h1>Chatbox</h1>
-      <Home/>
-      <button hidden={isLoggedIn} onClick={login}>LOGIN</button>
-      <button hidden={!isLoggedIn} onClick={logout}>LOGOUT</button>
+      <Home />
+      {isLoggedIn ? (<button onClick={logout}>LOGOUT</button>) : (<button onClick={login}>LOGIN</button>)}
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,21 +3,25 @@ import {addDoc, collection, getDocs} from "firebase/firestore";
 import {auth, db} from "../firebase-config";
 
 export default function Home() {
-    const [message,setMessage]=useState(localStorage.getItem("isLoggedIn"));
+    const [message,setMessage]=useState("");
     const [messageList,setMessageList]=useState([]);
     const collectionRef=collection(db,"messages");
+
+    const date=new Date();
+    const temp=[{message:"Hi",author:"Prakash",time:date},{message:"hello",author:"Dalla",time:date}];
+    // console.log(messageList);
 
     const sendMessage= async ()=>{
         await addDoc(collectionRef,{message,author:auth.currentUser.displayName,time:new Date()});
     }
 
-    useEffect(()=>{
-        const getMessages= async ()=>{
-            const data = await getDocs(collectionRef);
-            setMessageList(data.docs.map((item)=>({...item.data()})));
-        };
-        getMessages();
-    });
+    // useEffect(()=>{
+    //     const getMessages= async ()=>{
+    //         const data = await getDocs(collectionRef);
+    //         setMessageList(data.docs.map((item)=>({...item.data()})));
+    //     };
+    //     getMessages();
+    // });
 
     
   return (
