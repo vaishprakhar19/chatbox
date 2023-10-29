266794b2bb347389760ee6498b3014e881a6c92d
vaishprakhar19 <vaishprakhar19@gmail.com>
2023-10-29 11:32:52 +0530

    final

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,36 +9,37 @@ export default function Home() {
     const collectionRef=collection(db,"messages");
 
     const date=new Date();
-    const temp=[{message:"Hi",author:"Prakash",time:date},{message:"hello",author:"Dalla",time:date}];//TEMPORARY
+    // const temp=[{message:"Hi",author:"Prakash",time:date},{message:"hello",author:"Dalla",time:date}];//TEMPORARY
 
     const sendMessage= async ()=>{
         await addDoc(collectionRef,{message,author:auth.currentUser.displayName,time:new Date()});
     }
 
-    // useEffect(()=>{
-    //     const getMessages= async ()=>{
-    //         const data = await getDocs(collectionRef);
-    //         setMessageList(data.docs.map((item)=>({...item.data()})));
-    //     };
-    //     getMessages();
-    // });
+    useEffect(()=>{
+        const getMessages= async ()=>{
+            const data = await getDocs(collectionRef);
+            setMessageList(data.docs.map((item)=>({...item.data()})));
+        };
+        getMessages();
+    });
 
     
   return (
     <div>
       <div className='input-box'>
       <input id="input" type="textarea" onChange={(event)=>{setMessage(event.target.value)}}></input>
-      <button id="send-button" onClick={sendMessage}>✈️</button>
+      <button id="send-button" onClick={sendMessage}>✈</button>
       </div>
       {
-        // messageList.map((item)=>{ 
-        temp.map((item)=>{                                         /*temporary stuff*/ 
+        // temp.map((item)=>{                                         /*temporary stuff*/ 
+        messageList.map((item)=>{ 
             return <div className="message-box"> 
+                    <div className='author'>
+                        {item.author}:
+                        <br></br>
+                    </div>
                     <div className='message-text'>
                         {item.message}
-                    </div>
-                    <div className='author'>
-                        {item.author}
                     </div>
                 </div>
             })
