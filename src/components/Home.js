3df64c11644061462daa998e6edb68ea8e5eb947
vaishprakhar19 <vaishprakhar19@gmail.com>
2023-11-01 22:06:34 +0530

    lets take a risk

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import {addDoc, collection, getDocs} from "firebase/firestore";
-import {auth, db} from "../firebase-config";
+import React, { useEffect, useRef, useState } from 'react';
+import { addDoc, collection, getDocs, orderBy, query } from "firebase/firestore";
+import { auth, db } from "../firebase-config";
 import "./styles.css";
 
 export default function Home(props) {
-    const [message,setMessage]=useState("");
-    const [messageList,setMessageList]=useState([]);
-    const collectionRef=collection(db,"messages");
+  const [message, setMessage] = useState("");
+  const [date, setDate] = useState("");
+  const [messageList, setMessageList] = useState([]);
+  const collectionRef = collection(db, "messages");
 
-    // const temp=[{message:"Hi",author:"Prakash",time:date},{message:"hello",author:"Dalla"];//TEMPORARY
+  const sendMessage = async () => {
+    await addDoc(collectionRef, { message, author: auth.currentUser.displayName, date });
+  }
 
-    const sendMessage= async ()=>{
-        await addDoc(collectionRef,{message,author:auth.currentUser.displayName});
+  // const temp=[
+  //   {message:"Hi",author:"Mahesh Dalla",date:"12pm"}
+  // ]
+
+  
+  useEffect(() => {
+    if (props.isLoggedIn) {
+      const getMessages = async () => {
+        const data = await getDocs(query(collectionRef,orderBy('date')));
+        setMessageList(data.docs.map((item) => ({ ...item.data() })));
+      };
+      getMessages();
     }
+  },[messageList,setMessageList]);
 
-    useEffect(()=>{
-
-      if(props.isLoggedIn){
-        const getMessages= async ()=>{
-            const data = await getDocs(collectionRef);
-            setMessageList(data.docs.map((item)=>({...item.data()})));
-        };
-        getMessages();
-      }
-    });
-
-    
   return (
     <div>
       <div className='input-box'>
-      <input id="input" type="textarea" onChange={(event)=>{setMessage(event.target.value)}}></input>
-      <button id="send-button" onClick={sendMessage}>✈</button>
+        <input id="input" type="textarea" onChange={(event) => {
+          setMessage(event.target.value);
+          setDate(new Date().toString());
+        }}></input>
+        <button id="send-button" onClick={sendMessage}>✈</button>
       </div>
-      {
-        // temp.map((item)=>{                                         /*temporary stuff*/ 
-        messageList.map((item)=>{ 
-            return <div className="message-box"> 
-                    <div className='author'>
-                        {item.author}:
-                        <br></br>
-                    </div>
-                    <div className='message-text'>
-                        {item.message}
-                    </div>
-                </div>
-            })
+      <div className='messages-container'>
+      { 
+        messageList.map((item) => {
+        // temp.map((item) => {
+          return <div className="message-box">
+            <div className='author'>
+              {item.author}:
+              <br></br>
+            </div>
+            <div className='message-text'>
+              {item.message}
+            </div>
+          </div>
+        })
       }
+      </div>
     </div>
   )
 }
