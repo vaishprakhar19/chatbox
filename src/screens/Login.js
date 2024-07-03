75ef6f534a4e52c0915067ec7a2552e7b42801ca
Prakhar Vaish <126570561+vaishprakhar19@users.noreply.github.com>
2024-07-03 21:26:23 +0530

    da

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -17,7 +17,7 @@ const Login = ({ setIsLoggedIn }) => {
         <div className='login'>
             <div className='blur'></div>
             <div className='info'>
-                <h1 className='heading' data-aos="fade-down" data-aos-duration="1500">Chatbox</h1>
+                <h1 className='heading' data-aos="fade-down" data-aos-duration="1500">Pikochat</h1>
                 <p data-aos="fade-down" data-aos-duration="1500">A group chat for all, login, get along with other users all over the world, read messages, dive into ongoing topics, or suggest your own. Have fun...</p>
             <button className="button" data-aos="fade-up" data-aos-duration="1500" data-aos-offset="100" onClick={login}>Login</button>
             </div>
