61b0980e648a510bb5cc89d2c0580fba9a93c69c
Prakhar Vaish <126570561+vaishprakhar19@users.noreply.github.com>
2024-07-03 23:11:45 +0530

    ggg

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -25,15 +25,11 @@ export default function Home({ isLoggedIn }) {
     if (isLoggedIn) {
       const unsub = onSnapshot(query(collectionRef, orderBy("date")), (snapshot) => {
         setMessageList(snapshot.docs.map((item) => ({ ...item.data(), id: item.id })));
-        // scrollToBottom();
+        scrollToBottom();
       });
       return () => unsub(); // Cleanup subscription on unmount
     }
   }, [isLoggedIn,collectionRef]);
-
-  useEffect(() => {
-    scrollToBottom();
-  }, [messageList]);
 
   return (
     <div className='home'>
